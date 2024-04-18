ee4e521cf482a488c88c54ddba162e483e5b5e51
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-04-17 21:59:05 -0300

    fix: eliminating useless class and add new features

--- src/app/App.java
+++ src/app/App.java
@@ -1,5 +1,29 @@
 package app;
 
-public class App {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.TreeMap;
 
+public class App {
+	public static void main(String[] args) {
+		Map<String, Integer> votes = new TreeMap<String, Integer>();
+		String path = "C:\\Users\\User\\eclipse-workspace\\urn_counter\\src\\repository\\in.txt";
+		
+		try (BufferedReader bf = new BufferedReader(new FileReader(path))) {
+			String line = bf.readLine();
+			while(line!=null) {
+				String[] splitedLine = line.split(",");
+				String cadidate = splitedLine[0];
+				int vote = Integer.parseInt(splitedLine[1]);
+				votes.put(cadidate, vote);
+				line=bf.readLine();
+			}
+			for(String key: votes.keySet()) {
+				System.out.println(key+": "+votes.get(key));
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
--- src/entities/model/Politician.java
+++
@@ -1,5 +0,0 @@
-package entities.model;
-
-public class Politician {
-
-}
